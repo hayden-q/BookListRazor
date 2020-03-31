20f489ae1d8151f18c25ca4193af7ccd474ce000
Hayden Quinn <haydens.programming@gmail.com>
2020-03-31 19:05:50 +1000

    Implemented Delete Book functionality

--- src/Pages/BookList/Index.cshtml.cs
+++ src/Pages/BookList/Index.cshtml.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using BookListRazor.Data;
 using BookListRazor.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 
@@ -21,6 +22,21 @@ namespace BookListRazor.Pages.BookList
 		public async Task OnGet()
 		{
 			Books = await _db.Books.ToListAsync();
+		}
+
+		public async Task<IActionResult> OnPostDelete(int id)
+		{
+			var bookFromDb = await _db.Books.FindAsync(id);
+
+			if (bookFromDb is null)
+			{
+				return NotFound();
+			}
+
+			_db.Books.Remove(bookFromDb);
+			await _db.SaveChangesAsync();
+
+			return RedirectToPage("Index");
 		}
 	}
 }
